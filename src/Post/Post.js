a85be123d77e05d2466d581e15d209de9a7ef1b3
Khalid <khalidfarajdev@gmail.com>
2025-05-05 16:06:01 +0300

    postThing

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,11 +1,9 @@
 import './Post.css';
-import { HRTrimmed } from 'flowbite-react';
 export default function Post() {
   return (
     <div className={'post'}>
       <h1>This is the post title</h1>
-      <HRTrimmed />
-      <p>this is the post body</p>
+      <hr style={{ width: '95%', border: '0.1px solid black' }} />      <p>this is the post body</p>
     </div>
   );
 }
